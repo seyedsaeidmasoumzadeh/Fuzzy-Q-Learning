b492c6e102fe64f63b204cca31b45a7c5cb7fb1d
Seyed Saeid Masoumzadeh <masoumzadeh@gmail.com>
2017-10-02 14:00:35 +0100

    Update FIS

--- FIS.py
+++ FIS.py
@@ -9,11 +9,11 @@ class Build(object):
     def get_input(self):
         return self.list_of_input_variable
 
-    def get_number_of_rulls(self):
-        number_of_rulls = 1
+    def get_number_of_rules(self):
+        number_of_rules = 1
         for input_variable in self.list_of_input_variable:
-            number_of_rulls = (number_of_rulls * self.get_number_of_fuzzy_sets(input_variable))
-        return number_of_rulls
+            number_of_rules = (number_of_rules * self.get_number_of_fuzzy_sets(input_variable))
+        return number_of_rules
 
     def get_number_of_fuzzy_sets(self, input_variable):
         return len(input_variable.get_fuzzy_sets())
